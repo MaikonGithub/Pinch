9d709e492f25797a4358d513578a7f63214c9012
Maikon Ferreira <maikon.ferreira@apinteractive.com.br>
2022-12-28 23:11:34 -0300

    Criação do projeto
    adicionando arquivos do projeto

---
+++ Pinch/Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Maikon Ferreira on 28/12/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
