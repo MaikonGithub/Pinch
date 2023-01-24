1a8f7500da3cec6a891f8d2afd18323af5f1dcf7
Maikon Ferreira <maikon.ferreira@apinteractive.com.br>
2023-01-23 23:41:42 -0300

    finish drag gesture

--- Pinch/Pinch/Screen/ContentView.swift
+++ Pinch/Pinch/Screen/ContentView.swift
@@ -10,12 +10,27 @@ import SwiftUI
 struct ContentView: View {
     
     //MARK: - PROPERTY
-    
+    //properties that defines default state of modifiers
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
     
-    //MARK: - FUNCTION
+    //MARK: - FUNCTIONS
+    //Methods session
     
+    //MARK: - ZOOM IN
+       func zoomIn() {
+           return withAnimation(.spring()) {
+               imageScale = 5
+           }
+       }
+    //MARK: - RESET IMAGE
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
     //MARK: - CONTENT
     
     var body: some View {
@@ -29,19 +44,33 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
+                
                 //MARK: - 1. TAP GESTURE
                     .onTapGesture(count: 2, perform: {
                         if imageScale == 1 {
-                            withAnimation(.spring()) {
-                                imageScale = 5
-                            }
+                            zoomIn()
                         } else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })// end of tap gesture.
+                
+                //MARK: - 2. DRAG GESTURE
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            }
+                            .onEnded{_ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                                
+                            }
+                    )//end of drag gesture
             }//: END OF ZSTACK
             .navigationTitle("Pinch & Zoo")
             .navigationBarTitleDisplayMode(.inline)
@@ -54,7 +83,6 @@ struct ContentView: View {
         .navigationViewStyle(.stack)
     }
 }
-
 //MARK: - PREVIEW
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
